fae9c513fbaa79f46a842c802553b1be5a30cb8e
bradford barr <bradford_barr@brown.edu>
2019-06-11 15:42:32 -0400

    updates

---
+++ app/hello-world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import requests
+
+payload = {'text': 'Hello World!'}
+r = requests.get('https://artii.herokuapp.com/make', params=payload)
+print(r.text)
+
+import MySQLdb
+
+db = MySQLdb.connect(host='maria', user='root', passwd='root')
+print(db)
